b8fc2cb71df6615b16e95af150923a866977839e
liyanguo <lyg@meitu.com>
2018-02-05 11:39:44 +0800

    add config file

--- src/bin/crontab.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-0 15 * * * /usr/bin/env bash /home/lyg/code/user_tag/sync_tag/bin/start.sh `date +"\%Y\%m\%d"`
--- src/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# sync_tag dir
-SYNC_TAG_DIR=$(cd $(dirname ${0}) && pwd)
-
-# prepare
